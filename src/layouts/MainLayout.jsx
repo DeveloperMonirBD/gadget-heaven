450858255dcb4769b5c9975896afc6f99030c493
Monir <mrmonir0558@gmail.com>
2024-11-04 01:38:17 +0600

    footer section and json file create done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = () => {
             <Navbar />
 
             {/* Dunamic Section  */}
-            <div className="min-h-[calc(100vh-232px)] max-w-7xl mx-auto px-3 py-12">
+            <div className="min-h-[calc(100vh-397px)] max-w-7xl mx-auto px-3 py-12">
                 <Outlet />
             </div>
 
